d89290fbcac4b6f59dd29f41f0f062d7f46d58e7
Lucas Carvalho <lucasjazz02@gmail.com>
2022-04-21 14:05:35 -0300

    passo a passo criação de nova branch

--- continuacaoTreino.js
+++
@@ -1,4 +0,0 @@
-console.log("RECEBAAA!");
-console.log("Graças a Deus, pai!");
-
-console.log("fechou la!");
---
+++ criacaoDeBranch.js
@@ -0,0 +1,18 @@
+// PASSO A PASSO CRIAÇÃO DE BRANCHs GITHUB PELO TERMINAL
+
+//certifique-se de que seus arquivos da main estejam salvos
+
+//transfira seus arquivos para o github (passo a passo na branch main)
+
+//git checkout -b "nome da branch nova" - para criar uma branch
+
+//git branch para verificar a lista de branchs do seu repositorio
+
+//git checkout nome da branch criada - para trocar para o nova branch
+
+//estando na branch nova voce poderar alterar seus arquivos, testar novos codigos
+//sem se preocupar com os arquivos que ja estavam funcionando na branch anterior
+
+// para voltar para a sua branch
+
+//git checkout nome da branch principal
